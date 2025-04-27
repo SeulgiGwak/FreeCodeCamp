049cb7d8d166b5f8bf5a1ed03c1cb22547251660
Seulgi <seulgi.gwak.kr@gmail.com>
2025-04-27 11:44:16 +1000

    first commit

---
+++ JavaScript/lab-javascript-build-a-sentence-maker.js
@@ -0,0 +1,22 @@
+let adjective ="beautiful";
+let noun = "boy";
+let verb = "running";
+let place = "playground";
+let adjective2 = "huge";
+let noun2 = "cherry pie";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective+ " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a "+ place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "jumpy";
+noun = "Easter Bunny";
+verb = "jumping";
+place = "garden";
+adjective2 = "funny";
+noun2 = "spinach";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+console.log (secondStory);
+
+console.log("Second story: " + secondStory);
